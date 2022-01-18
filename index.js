0d500c35c1c9835fd957172a2d090ebd7a8ff1f8
david-lafontant <lafontant.david@gmail.com>
2022-01-17 19:59:46 -0500

    Fix linter errors

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import displayBook from './modules/display.js';
 import navigation from './modules/navigation.js';
 import { DateTime } from './node_modules/luxon/src/luxon.js';
 
-
 navigation();
 
 // Display the clock
@@ -14,45 +13,44 @@ setInterval(() => {
 }, 1000);
 
 // Event: Display Books
-  document.addEventListener('DOMContentLoaded', displayBook.displayBooks);
-  let increment = 0;
-  
-  // Event: Add a Book
-  document.querySelector('#formId').addEventListener('submit', (e) => {
-    // Prevent actual submit
-    e.preventDefault();
-  
-    // Get form values
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const id = increment;
-  
-    increment++;
-  
-    // Validate
-    if (title === '' || author === '') {
-      alert('Please fill in all fields');
-    } else {
-      // Instantiate book
-      const book = new Book(title, author, id);
-  
-      // Add Book to displayBook
-      displayBook.addBookToList(book);
-  
-      // Add book to store
-      Store.addBook(book);
-  
-      // Clear fields
-      displayBook.clearFields();
-    }
-  });
- 
-  // Event: Remove a Book
-  document.querySelector('#listBooks').addEventListener('click', (e) => {
-    // Remove book from displayBook
-    displayBook.deleteBook(e.target);
-  
-    // Remove book from store
-    Store.removeBook(e.target);
-  });
+document.addEventListener('DOMContentLoaded', displayBook.displayBooks);
+let increment = 0;
 
+// Event: Add a Book
+document.querySelector('#formId').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const id = increment;
+
+  increment++;
+
+  // Validate
+  if (title === '' || author === '') {
+    alert('Please fill in all fields');
+  } else {
+    // Instantiate book
+    const book = new Book(title, author, id);
+
+    // Add Book to displayBook
+    displayBook.addBookToList(book);
+
+    // Add book to store
+    Store.addBook(book);
+
+    // Clear fields
+    displayBook.clearFields();
+  }
+});
+
+// Event: Remove a Book
+document.querySelector('#listBooks').addEventListener('click', (e) => {
+  // Remove book from displayBook
+  displayBook.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(e.target);
+});
--- modules/store.js
+++ modules/store.js
@@ -1,31 +1,29 @@
 export default class Store {
-    static getBooks() {
-      let books;
-      if (localStorage.getItem('books') === null) {
-        books = [];
-      } else {
-        books = JSON.parse(localStorage.getItem('books'));
-      }
-  
-      return books;
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
-  
-    static addBook(book) {
-      const books = Store.getBooks();
-      books.push(book);
-      localStorage.setItem('books', JSON.stringify(books));
-    }
-  
-    static removeBook(elem) {
 
-      const books = Store.getBooks();
-      if(books.length === 1) {
-        books.pop();
-      }
-      else{
-        books.splice(parseInt(elem.id), 1);
-      }
-      
-      localStorage.setItem('books', JSON.stringify(books));
-    }
+    return books;
   }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(elem) {
+    const books = Store.getBooks();
+    if (books.length === 1) {
+      books.pop();
+    } else {
+      books.splice(parseInt(elem.id), 1);
+    }
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
